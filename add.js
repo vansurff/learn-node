e3cbcf8a9492d1c1b0def681362cee2e7d6e12a4
Ivan Dela Cruz <vansurff@yahoo.com>
2018-11-04 19:11:01 -0800

    feat: add and variable

---
+++ add.js
@@ -0,0 +1,5 @@
+// add x and y and print it out
+x = 'abc'
+y = '2'
+console.log(x + y)
+console.log(y + x)
---
+++ variable.js
@@ -0,0 +1,8 @@
+// 1. what value do i wanna store
+// 2. what type of value is it
+// 3. what should i name my variable
+
+x = 'hey'
+console.log(x)
+y = 1
+console.log(y)
